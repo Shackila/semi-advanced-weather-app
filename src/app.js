ee26d3b877502bf386428b5ed7851be0171e1293
Shackila <mvd.shakila@gmail.com>
2022-06-06 11:57:46 +0430

    app.js was updated

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ let sixthDay = days[now.getDay() + 5];
 
 let seventhDay = days[now.getDay() + 6];
 
-function displayForcast() {
+function displayForcast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row row-cols-1 row-cols-md-6">`;
   let realDays = [
@@ -73,6 +73,12 @@ function displayForcast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "2fa6e56b10efce71f24cca911445a98c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemprature(response) {
   let cityElement = document.querySelector("#city-name");
   let tempratureElement = document.querySelector("#big-tempreture");
@@ -90,6 +96,7 @@ function displayTemprature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+    getForecast(response.data.coord);
 }
 
 function search(city) {
